ae6f5090a0917e14ee0a85b2fbd5fc718ed262c8
Michael Koontz <mikoontz@gmail.com>
2017-06-12 17:48:33 -0700

    don't track big files

--- src/data_carpentry/create_conifer_mask.R
+++ src/data_carpentry/create_conifer_mask.R
@@ -1,14 +1,26 @@
 library(raster)
 library(dplyr)
 library(sf)
+library(fasterize)
 
-lookup <- read.csv("data/features/landcover_LANDFIRE/landfire_biophysical_settings_lookup_table.csv")
-bps_sn <- raster("data/features/landcover_LANDFIRE/landfire_biophysical_settings_sn.tif")
+r <- raster("data/features/sierra_nevada_30m_landsat_template.tif")
+sn <- st_read("data/features/SierraEcoregion_Jepson/") %>%
+  st_transform(proj4string(r))
 
-nsn <- st_read("data/features/fire_return_interval_departure/FRID_NorthSierra15_1.gdb/")
+nsn <- st_read("data/features/fire_return_interval_departure/FRID_NorthSierra15_1.gdb/") %>%
+  st_transform(proj4string(r)) %>%
+  filter(PFR %in% c("Dry mixed conifer", "Moist mixed conifer", "Yellow pine"))
 
-colnames(nsn)
-nsn[1:5, ]
-forest <- c()
+ssn <- st_read("data/features/fire_return_interval_departure/FRID_SouthSierra15_1.gdb/") %>%
+  st_transform(proj4string(r)) %>%
+  filter(PFR %in% c("Dry mixed conifer", "Moist mixed conifer", "Yellow pine"))
 
-unique(nsn$PFR)
+nsn_r <- fasterize(sf = nsn, raster = r)
+ssn_r <- fasterize(sf = ssn, raster = r)
+
+sn_r <- raster::merge(nsn_r, ssn_r)
+
+plot(sn_r)
+plot(sn$geometry, add = TRUE)
+
+writeRaster(x = sn_r, filename = "data/features/landcover_PFR/mixed_conifer.tif")
