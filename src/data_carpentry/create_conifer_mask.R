7c289ef7d7d7e18ffe03d39f64bfdf9e2420dfdd
Michael Koontz <mikoontz@gmail.com>
2017-06-14 16:32:09 -0700

    added code to write a shapefile version of the conifer mask in order to use (possibly?) or at least have the flexibility to use a vector format of these data, rather than a rasterized version.

--- src/data_carpentry/create_conifer_mask.R
+++ src/data_carpentry/create_conifer_mask.R
@@ -15,6 +15,16 @@ ssn <- st_read("data/features/fire_return_interval_departure/FRID_SouthSierra15_
   st_transform(proj4string(r)) %>%
   filter(PFR %in% c("Dry mixed conifer", "Moist mixed conifer", "Yellow pine"))
 
+# Directly combine the vector shapes into a single, large vector shape
+sn_v <- rbind(nsn, ssn)
+st_write(obj = sn_v, dsn = "data/features/landcover_PFR/mixed_conifer/mixed_conifer.shp", driver = "ESRI Shapefile")
+# Successful write; some warnings about truncated column names and truncated
+# values in the shape_area column (we can always recalculate these)
+# This shapefile was uploaded to Google Earth Engine and is available here:
+#
+
+
+# Use fasterize to turn the vector shapes into rasters
 nsn_r <- fasterize(sf = nsn, raster = r)
 ssn_r <- fasterize(sf = ssn, raster = r)
 
