f8084e041c02cff9ba85b230ad939cdd2a33d9d5
Michael Koontz <mikoontz@gmail.com>
2017-06-15 18:53:17 -0700

    Full makeover of heterogeneity/severity code to make it more modular and easier to modify for other data sources [e.g., using other Landsat satellite time series, using the CBI data]. Also a full revamp of the annotations for readability, especially since some folks may not have access to GEE in order to see the code itself, and are just seeing it through GitHub (or the like). I also took full advantage of the new Table as Asset feature of Google Earth Engine which allows direct upload of shapefiles without a (sometimes lossy) translation to a .kml file first

---
+++ ee-remote-sensing-resistance/create_raster_template.js
@@ -0,0 +1,31 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var l5sr = ee.ImageCollection("LANDSAT/LT5_SR"),
+    sn = ee.FeatureCollection("ft:1vdDUTu09Rkw5qKR_DSfmFX-b_7kqy4E-pjxg9Sq6");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+var landsat = ee.Image(l5sr.filterDate("1985-07-27", "1986-07-27").
+                select(["B1"]).
+                first());
+
+var mask = ee.Image(1).clip(sn);
+
+var template_img = landsat.
+                    unmask().
+                    addBands(mask).
+                    rename(["B1", "mask"]).
+                    select(["mask"]).
+                    clip(sn);
+              
+Map.addLayer(mask);                    
+Map.addLayer(template_img);
+
+Map.addLayer(landsat.clip(sn.geometry().bounds()), {min: -2000, max: 10000, palette: ["ff0000", "00ff00"]});
+Map.centerObject(sn);
+
+Export.image.toDrive({image: template_img,
+                      description: "sierra_nevada_30m_landsat_template",
+                      fileNamePrefix: "sierra_nevada_30m_landsat_template",
+                      scale: 30,
+                      region: sn.geometry().getInfo()['coordinates'],
+                      maxPixels: 3000000000,
+                      folder: "ee"
+});
