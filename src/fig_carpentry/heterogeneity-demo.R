c6398c7f544a4d1929b9d0b8604911df31d11be5
Michael Koontz <mikoontz@gmail.com>
2018-02-02 16:06:10 -0800

    initial commit of code to generate heterogeneity demonstration figure using USDA Farm Service Agency aerial imagery

---
+++ src/fig_carpentry/heterogeneity-demo.R
@@ -0,0 +1,134 @@
+library(png)
+library(sf)
+library(dplyr)
+library(viridis)
+
+# Where were some of the highest and lowest heterogeneity samples?
+samps1 <- st_read("data/fire_samples/fires-strat-samples_48-day-window_L45_none-interp.geojson")
+samps2 <- st_read("data/fire_samples/fires-strat-samples_48-day-window_L578_none-interp.geojson")
+samps3 <- st_read("data/fire_samples/fires-strat-samples_48-day-window_L78_none-interp.geojson")
+s <- rbind(samps1, samps2, samps3)
+
+# sn <- st_read("data/features/SierraEcoregion_Jepson/SierraEcoregion_Jepson.shp") %>% st_transform(4326)
+# 
+# plot(density(s$het_ndvi_1))
+# sort(unique(s$year_))
+# ndvi_midrange <- quantile(s$preFire_ndvi, probs = c(0.25, 0.75))
+# 
+# plot(sn$geometry)
+# five_low <-
+#   s %>% 
+#   filter(conifer_forest == 1) %>% 
+#   filter(preFire_ndvi > ndvi_midrange[1] & preFire_ndvi < ndvi_midrange[2]) %>% 
+#   # filter(het_ndvi_1 < 0.15) %>%
+#   filter(as.numeric(as.character(year_)) > 2000) %>%
+#   arrange(het_ndvi_1) %>% 
+#   head(n = 5) %>% 
+#   select(het_ndvi_1, het_ndvi_2, het_ndvi_3, het_ndvi_4, elev, slope, aspect, alarm_date, year_, focal_mean_ndvi_1, focal_mean_ndvi_2, focal_mean_ndvi_3, focal_mean_ndvi_4, preFire_ndvi, fire_name, id)
+# 
+# five_high <-
+#   s %>% 
+#   filter(conifer_forest == 1) %>% 
+#   filter(preFire_ndvi > ndvi_midrange[1] & preFire_ndvi < ndvi_midrange[2]) %>% 
+#   # filter(het_ndvi_1 < 0.15) %>% 
+#   filter(as.numeric(as.character(year_)) > 2000) %>% 
+#   arrange(-het_ndvi_1) %>% 
+#   head(n = 5) %>% 
+#   select(het_ndvi_1, het_ndvi_2, het_ndvi_3, het_ndvi_4, elev, slope, aspect, alarm_date, year_, focal_mean_ndvi_1, focal_mean_ndvi_2, focal_mean_ndvi_3, focal_mean_ndvi_4, preFire_ndvi, fire_name, id)
+
+
+# st_write(five_low, "data/fire_samples/five-low.kml", layer = "fire_samps")
+# st_write(five_high, "data/fire_samples/five-high.kml", layer = "fire_samps")
+
+d <- st_read("data/heterogeneity-demo.geojson")
+glimpse(d)
+
+sup1 <- readPNG("figs/heterogeneity-demo/suppressed_1-pixel.png")
+sup2 <- readPNG("figs/heterogeneity-demo/suppressed_2-pixel.png")
+sup3 <- readPNG("figs/heterogeneity-demo/suppressed_3-pixel.png")
+sup4 <- readPNG("figs/heterogeneity-demo/suppressed_4-pixel.png")
+man1 <- readPNG("figs/heterogeneity-demo/fire-use_1-pixel.png")
+man2 <- readPNG("figs/heterogeneity-demo/fire-use_2-pixel.png")
+man3 <- readPNG("figs/heterogeneity-demo/fire-use_3-pixel.png")
+man4 <- readPNG("figs/heterogeneity-demo/fire-use_4-pixel.png")
+
+img_mar <- c(0.5, 0.5, 0.5, 0.5)
+histo_mar <- c(4, 3, 0.5, 0)
+box_width <- 4
+
+# plot_mat <- matrix(1:12, nrow = 3, ncol = 4, byrow = TRUE)
+# layout(plot_mat, respect = TRUE)
+pdf("figs/heterogeneity-imagery-histograms.pdf", width = 17.3 / 2.54, height = 0.75 * 17.3 / 2.54)
+par(mfrow = c(3, 4), mar = img_mar, xaxs = 'i', yaxs='i', oma = c(2.5, 2.5, 2.5, 2.5))
+my_colors <- viridis(2)
+
+#### Plot fire use images 
+plot(x = 0, y = 0, xlim = c(0, 10), ylim = c(0, 10), type = "n", asp = 1, axes = FALSE, xlab = NA, ylab = NA)
+rasterImage(man1, xleft = 0, ybottom = 0, xright = 10, ytop = 10)
+box(which = "plot", lwd = box_width, col = my_colors[1])
+mtext(c("90m x 90m", "150m x 150m", "210m x 210m", "270m x 270m"), 
+      side = 3, outer = TRUE, at = c(1/8, 3/8, 5/8, 7/8))
+
+plot(x = 0, y = 0, xlim = c(0, 10), ylim = c(0, 10), type = "n", asp = 1, axes = FALSE, xlab = NA, ylab = NA)
+rasterImage(man2, xleft = 0, ybottom = 0, xright = 10, ytop = 10)
+box(which = "plot", lwd = box_width, col = my_colors[1])
+
+plot(x = 0, y = 0, xlim = c(0, 10), ylim = c(0, 10), type = "n", asp = 1, axes = FALSE, xlab = NA, ylab = NA)
+rasterImage(man3, xleft = 0, ybottom = 0, xright = 10, ytop = 10)
+box(which = "plot", lwd = box_width, col = my_colors[1])
+
+plot(x = 0, y = 0, xlim = c(0, 10), ylim = c(0, 10), type = "n", asp = 1, axes = FALSE, xlab = NA, ylab = NA)
+rasterImage(man4, xleft = 0, ybottom = 0, xright = 10, ytop = 10)
+box(which = "plot", lwd = box_width, col = my_colors[1])
+
+#### Plot suppressed images ####
+
+plot(x = 0, y = 0, xlim = c(0, 10), ylim = c(0, 10), type = "n", asp = 1, axes = FALSE, xlab = NA, ylab = NA)
+rasterImage(sup1, xleft = 0, ybottom = 0, xright = 10, ytop = 10)
+box(which = "plot", lwd = box_width, col = my_colors[2])
+
+plot(x = 0, y = 0, xlim = c(0, 10), ylim = c(0, 10), type = "n", asp = 1, axes = FALSE, xlab = NA, ylab = NA)
+rasterImage(sup2, xleft = 0, ybottom = 0, xright = 10, ytop = 10)
+box(which = "plot", lwd = box_width, col = my_colors[2])
+
+plot(x = 0, y = 0, xlim = c(0, 10), ylim = c(0, 10), type = "n", asp = 1, axes = FALSE, xlab = NA, ylab = NA)
+rasterImage(sup3, xleft = 0, ybottom = 0, xright = 10, ytop = 10)
+box(which = "plot", lwd = box_width, col = my_colors[2])
+
+plot(x = 0, y = 0, xlim = c(0, 10), ylim = c(0, 10), type = "n", asp = 1, axes = FALSE, xlab = NA, ylab = NA)
+rasterImage(sup4, xleft = 0, ybottom = 0, xright = 10, ytop = 10)
+box(which = "plot", lwd = box_width, col = my_colors[2])
+
+#### Plot histograms ####
+par(mar = histo_mar)
+
+plot(density(s$het_ndvi_1), main = NA, xaxt = "n", las = 1, xlab = NA, ylab = NA)
+axis(side = 1, at = d$het_ndvi_1, labels = round(d$het_ndvi_1, 2), las = 2, font = 2)
+axis(side = 1, at = c(0, max(s$het_ndvi_1 / 2), max(s$het_ndvi_1)), labels = c(0, round(max(s$het_ndvi_1 / 2), 2), round(max(s$het_ndvi_1), 2)), las = 2)
+abline(v = d[d$type == "fire_use", "het_ndvi_1"], col = my_colors[1], lwd = 2)
+abline(v = d[d$type == "suppressed", "het_ndvi_1"], col = my_colors[2], lwd = 2)
+legend("topright", legend = c("Groupy-gappy forest", "Dense, homogenous forest"), col = my_colors, lty = 1, lwd = 3, bty = "n")
+mtext(side = 2, text = "Density", outer = TRUE, at = 1/6, cex = 1)
+
+plot(density(s$het_ndvi_2), main = NA, xaxt = "n", las = 1, xlab = NA, ylab = NA)
+axis(side = 1, at = d$het_ndvi_2, labels = round(d$het_ndvi_2, 2), las = 2, font = 2)
+axis(side = 1, at = c(0, max(s$het_ndvi_2 / 2), max(s$het_ndvi_2)), labels = c(0, round(max(s$het_ndvi_2 / 2), 2), round(max(s$het_ndvi_2), 2)), las = 2)
+abline(v = d[d$type == "fire_use", "het_ndvi_2"], col = my_colors[1], lwd = 2)
+abline(v = d[d$type == "suppressed", "het_ndvi_2"], col = my_colors[2], lwd = 2)
+# legend("topright", legend = c("Groupy-gappy forest", "Dense, homogenous forest"), col = my_colors, lty = 1, lwd = 3)
+
+plot(density(s$het_ndvi_3), main = NA, xaxt = "n", las = 1, xlab = NA, ylab = NA)
+axis(side = 1, at = d$het_ndvi_3, labels = round(d$het_ndvi_3, 2), las = 2, font = 2)
+axis(side = 1, at = c(0, max(s$het_ndvi_3 / 2), max(s$het_ndvi_3)), labels = c(0, round(max(s$het_ndvi_3 / 2), 2), round(max(s$het_ndvi_3), 2)), las = 2)
+abline(v = d[d$type == "fire_use", "het_ndvi_3"], col = my_colors[1], lwd = 2)
+abline(v = d[d$type == "suppressed", "het_ndvi_3"], col = my_colors[2], lwd = 2)
+# legend("topright", legend = c("Groupy-gappy forest", "Dense, homogenous forest"), col = my_colors, lty = 1, lwd = 3)
+
+plot(density(s$het_ndvi_4), main = NA, xaxt = "n", las = 1, xlab = NA, ylab = NA)
+axis(side = 1, at = d$het_ndvi_4, labels = round(d$het_ndvi_4, 2), las = 2, font = 2)
+axis(side = 1, at = c(0, max(s$het_ndvi_4 / 2), max(s$het_ndvi_4)), labels = c(0, round(max(s$het_ndvi_4 / 2), 2), round(max(s$het_ndvi_4), 2)), las = 2)
+abline(v = d[d$type == "fire_use", "het_ndvi_4"], col = my_colors[1], lwd = 2)
+abline(v = d[d$type == "suppressed", "het_ndvi_4"], col = my_colors[2], lwd = 2)
+# legend("topright", legend = c("Groupy-gappy forest", "Dense, homogenous forest"), col = my_colors, lty = 1, lwd = 3)
+mtext(side = 1, text = "Heterogeneity of NDVI", outer = TRUE, line = 0, cex = 1)
+dev.off()
