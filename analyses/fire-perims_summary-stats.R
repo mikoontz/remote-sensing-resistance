7df006071d85fd9a72404b7d0b1acadc46070889
Michael Koontz <mikoontz@gmail.com>
2018-02-21 16:17:20 -0800

    separate the filter bounds of sierra nevada on the usfs region 5 geospatial fire perimeter data set

--- analyses/fire-perims_summary-stats.R
+++ analyses/fire-perims_summary-stats.R
@@ -9,21 +9,11 @@ frap <- st_read(dsn = "data/features/fire_perim/fire_perim_sn_16_1_shp/",
                  stringsAsFactors = FALSE) %>% 
   st_transform(4326)
 
-usfs <- st_read(dsn = "data/features/fire_perim/veg_severity_perimeters16_1.gdb/",
-                stringsAsFactors = FALSE) %>% 
-  mutate(geometry = st_sfc(Shape)) %>% 
-  st_set_geometry("geometry") %>% 
-  select(-Shape) %>% 
-  st_transform(4326)
-
-sn <- st_read("data/features/SierraEcoregion_Jepson/SierraEcoregion_Jepson.shp") %>% 
-  st_transform(4326)
-
-usfs_sn <- st_intersection(x = usfs, y = sn)
-# 
-# frap %>%
-#   filter(!is.na(ALARM_DATE)) %>% 
-#   dim()
+if (file.exists("data/data_output/usfs-fire-perimeters-sn.rds")) {
+  load("data/data_output/usfs-fire-perimeters-sn.rds")
+} else {
+  source("data/data_carpentry/filter-sn_usfs_fire_perims.R")
+}
 
 if (file.exists("data/data_output/all-fire-samples.rds")) {
   load("data/data_output/all-fire-samples.rds")
@@ -33,7 +23,10 @@ if (file.exists("data/data_output/all-fire-samples.rds")) {
 
 samps
 
-total_conifer_samps <- nrow(samps %>% filter(conifer_forest == 1))
+total_conifer_samps <- 
+  samps %>% 
+  filter(conifer_forest == 1) %>% 
+  nrow()
 
 s <-
   samps %>%
