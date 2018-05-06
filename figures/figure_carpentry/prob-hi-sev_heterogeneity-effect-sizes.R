37ddfebf0eabc383d9620bfc9260f3861789347c
Michael Koontz <mikoontz@gmail.com>
2018-05-06 11:20:21 -0700

    generate a figure of posterior and prior distributions that puts extreme and normal conditions on same plot

--- figures/figure_carpentry/prob-hi-sev_heterogeneity-effect-sizes.R
+++ figures/figure_carpentry/prob-hi-sev_heterogeneity-effect-sizes.R
@@ -2,6 +2,10 @@
 # based on heterogeneity, 100 hour fuel moisture, whether fuel moisture 
 # conditions are extreme, topographic ruggedness, a proxy for canopy density
 
+# From Ecology Letters figure guidelines, to give an example:
+# single column (82 mm), two-thirds page width (110 mm) or full page width (173 mm)
+
+
 library(sf)
 library(dplyr)
 library(ggplot2)
@@ -108,9 +112,10 @@ m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_prior_betas <- m_sevOrNot_extr
 het_normal_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_prior <- apply(m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_prior_betas, 1, function(x) x %*% het_normal)
 het_xtreme_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_prior <- apply(m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_prior_betas, 1, function(x) x %*% het_xtreme)
 
-
+dev.off()
 # Probability density plots of the effect of heterogeneity under normal and extreme 100-hour fuel moisture conditions
-par(mfrow = c(1, 4))
+pdf(here::here("figures/heterogeneity-effect-by-fuel-condition.pdf"), width = (17.3 / 2.54), height = (17.3 / 2.54 / 2))
+par(mfrow = c(1, 5), oma = c(2, 3, 0, 0), mar = c(4, 1.5, 4, 1.5))
 # 1 pixel neighborhood
 het_norm_1_dens <- density(het_normal_sevOrNot_extremeFm100AndRawFm100_1_ssBurned)
 het_xtrm_1_dens <- density(het_xtreme_sevOrNot_extremeFm100AndRawFm100_1_ssBurned)
@@ -118,7 +123,7 @@ het_norm_1_prior_dens <- density(het_normal_sevOrNot_extremeFm100AndRawFm100_1_s
 het_xtrm_1_prior_dens <- density(het_xtreme_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_prior)
 
 het_1_dens_xlim <- range(c(het_norm_1_dens$x, het_xtrm_1_dens$x))
-plot(het_norm_1_dens, xlim = het_1_dens_xlim, col = "blue", xlab = "Effect size of heterogeneity of neighborhood NDVI on fire severity", ylab = "Probability density", las = 1, main = NA)
+plot(het_norm_1_dens, xlim = het_1_dens_xlim, col = "blue", xlab = NA, ylab = NA, las = 1, main = NA)
 abline(v = mean(het_normal_sevOrNot_extremeFm100AndRawFm100_1_ssBurned), col = "blue", lwd = 2)
 lines(het_xtrm_1_dens, col = "red")
 abline(v = mean(het_xtreme_sevOrNot_extremeFm100AndRawFm100_1_ssBurned), col = "red", lwd = 2)
@@ -127,8 +132,9 @@ abline(v = mean(het_xtreme_sevOrNot_extremeFm100AndRawFm100_1_ssBurned), col = "
 lines(het_norm_1_prior_dens, lty = 2, col = "blue")
 lines(het_xtrm_1_prior_dens, lty = 2, col = "red")
 
-abline(v = 0, lwd = 2, lty = 1)
-legend("topright", legend = c("Normal fuel moisture", "Extreme fuel moisture"), col = c("blue", "red"), bty = "n", lty = 1, lwd = 3)
+# abline(v = 0, lwd = 2, lty = 1)
+mtext(side = 3, text = "90m x 90m\nneighborhood", line = 0.5)
+# legend("topright", legend = c("Normal fuel moisture", "Extreme fuel moisture"), col = c("blue", "red"), bty = "n", lty = 1, lwd = 3)
 
 # 2 pixel neighborhood
 het_norm_2_dens <- density(het_normal_sevOrNot_extremeFm100AndRawFm100_2_ssBurned)
@@ -137,17 +143,18 @@ het_norm_2_prior_dens <- density(het_normal_sevOrNot_extremeFm100AndRawFm100_2_s
 het_xtrm_2_prior_dens <- density(het_xtreme_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_prior)
 
 het_2_dens_xlim <- range(c(het_norm_2_dens$x, het_xtrm_2_dens$x))
-plot(het_norm_2_dens, xlim = het_2_dens_xlim, col = "blue", xlab = "Effect size of heterogeneity of neighborhood NDVI on fire severity", ylab = "Probability density", las = 1, main = NA)
+plot(het_norm_2_dens, xlim = het_2_dens_xlim, col = "blue", xlab = NA, las = 1, main = NA, ylab = NA)
 abline(v = mean(het_normal_sevOrNot_extremeFm100AndRawFm100_2_ssBurned), col = "blue", lwd = 2)
 lines(het_xtrm_2_dens, col = "red")
 abline(v = mean(het_xtreme_sevOrNot_extremeFm100AndRawFm100_2_ssBurned), col = "red", lwd = 2)
 
 # Plot priors
-lines(het_norm_2_prior_dens)
-lines(het_xtrm_2_prior_dens)
+lines(het_norm_2_prior_dens, lty = 2, col = "blue")
+lines(het_xtrm_2_prior_dens, lty = 2, col = "red")
 
-abline(v = 0, lwd = 2)
-legend("topright", legend = c("Normal fuel moisture", "Extreme fuel moisture"), col = c("blue", "red"), bty = "n", lty = 1, lwd = 3)
+# abline(v = 0, lwd = 2)
+mtext(side = 3, text = "150m x 150m\nneighborhood", line = 0.5)
+# legend("topright", legend = c("Normal fuel moisture", "Extreme fuel moisture"), col = c("blue", "red"), bty = "n", lty = 1, lwd = 3)
 
 # 3 pixel neighborhood
 het_norm_3_dens <- density(het_normal_sevOrNot_extremeFm100AndRawFm100_3_ssBurned)
@@ -156,17 +163,18 @@ het_norm_3_prior_dens <- density(het_normal_sevOrNot_extremeFm100AndRawFm100_3_s
 het_xtrm_3_prior_dens <- density(het_xtreme_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_prior)
 
 het_3_dens_xlim <- range(c(het_norm_3_dens$x, het_xtrm_3_dens$x))
-plot(het_norm_3_dens, xlim = het_3_dens_xlim, col = "blue", xlab = "Effect size of heterogeneity of neighborhood NDVI on fire severity", ylab = "Probability density", las = 1, main = NA)
+plot(het_norm_3_dens, xlim = het_3_dens_xlim, col = "blue", xlab = NA, las = 1, main = NA, ylab = NA)
 abline(v = mean(het_normal_sevOrNot_extremeFm100AndRawFm100_3_ssBurned), col = "blue", lwd = 2)
 lines(het_xtrm_3_dens, col = "red")
 abline(v = mean(het_xtreme_sevOrNot_extremeFm100AndRawFm100_3_ssBurned), col = "red", lwd = 2)
 
 # Plot priors
-lines(het_norm_3_prior_dens)
-lines(het_xtrm_3_prior_dens)
+lines(het_norm_3_prior_dens, col = "blue", lty = 2)
+lines(het_xtrm_3_prior_dens, col = "red", lty = 2)
 
-abline(v = 0, lwd = 2)
-legend("topright", legend = c("Normal fuel moisture", "Extreme fuel moisture"), col = c("blue", "red"), bty = "n", lty = 1, lwd = 3)
+# abline(v = 0, lwd = 2)
+mtext(side = 3, text = "210m x 210m\nneighborhood", line = 0.5)
+# legend("topright", legend = c("Normal fuel moisture", "Extreme fuel moisture"), col = c("blue", "red"), bty = "n", lty = 1, lwd = 3)
 
 # 4 pixel neighborhood
 het_norm_4_dens <- density(het_normal_sevOrNot_extremeFm100AndRawFm100_4_ssBurned)
@@ -175,15 +183,24 @@ het_norm_4_prior_dens <- density(het_normal_sevOrNot_extremeFm100AndRawFm100_4_s
 het_xtrm_4_prior_dens <- density(het_xtreme_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_prior)
 
 het_4_dens_xlim <- range(c(het_norm_4_dens$x, het_xtrm_4_dens$x))
-plot(het_norm_4_dens, xlim = het_4_dens_xlim, col = "blue", xlab = "Effect size of heterogeneity of neighborhood NDVI on fire severity", ylab = "Probability density", las = 1, main = NA)
+plot(het_norm_4_dens, xlim = het_4_dens_xlim, col = "blue", xlab = NA, las = 1, main = NA, ylab = NA)
 abline(v = mean(het_normal_sevOrNot_extremeFm100AndRawFm100_4_ssBurned), col = "blue", lwd = 2)
 lines(het_xtrm_4_dens, col = "red")
 abline(v = mean(het_xtreme_sevOrNot_extremeFm100AndRawFm100_4_ssBurned), col = "red", lwd = 2)
 
 # Plot priors
-lines(het_norm_4_prior_dens)
-lines(het_xtrm_4_prior_dens)
+lines(het_norm_4_prior_dens, lty = 2, col = "blue")
+lines(het_xtrm_4_prior_dens, lty = 2, col = "red")
 
-abline(v = 0, lwd = 2)
-legend("topright", legend = c("Normal fuel moisture", "Extreme fuel moisture"), col = c("blue", "red"), bty = "n", lty = 1, lwd = 3)
+# abline(v = 0, lwd = 2)
+mtext(side = 3, text = "270m x 270m\nneighborhood", line = 0.5)
 
+plot(1, 1, xlim = c(2, 4), axes = FALSE, bty = "n", xlab = NA, ylab = NA)
+
+legend("left", legend = c("Normal fuel moisture", "Extreme fuel moisture", "Posterior distribution", "Prior distribution"), col = c("blue", "red", "black", "black"), bty = "n", lty = c(NA, NA, 1, 2), lwd = c(NA, NA, 2, 2), pch = c(15, 15, NA, NA), inset = c(-0.40), xpd = NA)
+
+
+mtext(side = 1, text = "Effect of heterogeneity of neighborhood NDVI\non the log-odds of a high severity fire", outer = TRUE, at = 0.4, line = 0.5)
+mtext(side = 2, text = "Probability density", outer = TRUE, line = 1.5)
+
+dev.off()
