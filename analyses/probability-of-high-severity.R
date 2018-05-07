421927eaf8f01f0c1836e5bcee42c74da6993846
Michael Koontz <mikoontz@gmail.com>
2018-05-07 14:45:01 -0700

    wrap reused code in a simpler function; makes it easier to export the code and use the resulting variables dynamically in the manuscript.

--- analyses/probability-of-high-severity.R
+++ analyses/probability-of-high-severity.R
@@ -2,13 +2,13 @@
 # based on heterogeneity, 100 hour fuel moisture, whether fuel moisture 
 # conditions are extreme, topographic ruggedness, a proxy for canopy density
 
+library(ggplot2)
+library(brms)
+library(tidyr)
 library(lubridate)
 library(sf)
 library(dplyr)
-library(ggplot2)
 library(here)
-library(lubridate)
-library(brms)
 
 if (!file.exists(here::here("analyses/analyses_output/m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm.rds")) |
     !file.exists(here::here("analyses/analyses_output/m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm.rds")) |
@@ -36,107 +36,49 @@ fm100_s_means <-
   group_by(extreme80_fm100) %>% 
   summarize(mean_fm100 = mean(fm100_s))
 
-het_normal <- c(0, 1, 0, pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 0), mean_fm100)), 0, 0, 0, 0, pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 0), mean_fm100)), 0, 0)
 
-het_xtreme <- c(0, 1, 1, pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 1), mean_fm100))
-                , 0, 0, 0, 1, pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 1), mean_fm100))
-                , pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 1), mean_fm100))
-                , pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 1), mean_fm100))
-)
+get_beta_coef <- function(m, fm100_s_means) {
+  # Output will be a distribution for Intercept, heterogeneity in normal conditions, heterogeneity in extreme conditions, fm100 in normal conditions, fm100 in extreme conditions, preFire_ndvi, topo_roughness, and pahl
+  samps <- posterior_samples(m)
+  prior_samps <- prior_samples(m)
+  
+  betas <- samps[, 1:11]
+  betas_prior <- prior_samps[, 1:11]
+  
+  het_normal <- c(0, 1, 0, pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 0), mean_fm100)), 0, 0, 0, 0, pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 0), mean_fm100)), 0, 0)
+  
+  het_xtreme <- c(0, 1, 1, pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 1), mean_fm100))
+                  , 0, 0, 0, 1, pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 1), mean_fm100))
+                  , pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 1), mean_fm100))
+                  , pull(dplyr::select(filter(fm100_s_means, extreme80_fm100 == 1), mean_fm100)))
+  
+  fm100_xtreme <- c(0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0)
+  
+  # coefficients
+  
+  b_intercept <- betas$b_Intercept
+  b_het_normal <- apply(betas, 1, function(x) x %*% het_normal)
+  b_het_xtreme <- apply(betas, 1, function(x) x %*% het_xtreme)
+  b_fm100_normal <- betas$b_fm100_s
+  b_fm100_xtreme <- apply(betas, 1, function(x) x %*% fm100_xtreme)
+  b_preFire_ndvi <- betas$b_preFire_ndvi_s
+  b_topo_roughness <- dplyr::select(betas, starts_with("b_topo_roughness")) %>% pull()
+  b_pahl <- betas$b_pahl_s
+  
+  b_intercept_prior <- betas_prior$b_Intercept
+  b_het_normal_prior <- apply(betas_prior, 1, function(x) x %*% het_normal)
+  b_het_xtreme_prior <- apply(betas_prior, 1, function(x) x %*% het_xtreme)
+  b_fm100_normal_prior <- betas_prior$b_fm100_s
+  b_fm100_xtreme_prior <- apply(betas_prior, 1, function(x) x %*% fm100_xtreme)
+  b_preFire_ndvi_prior <- betas_prior$b_preFire_ndvi_s
+  b_topo_roughness_prior <- dplyr::select(betas_prior, starts_with("b_topo_roughness")) %>% pull()
+  b_pahl_prior <- betas_prior$b_pahl_s
+  
+  data.frame(b_intercept, b_het_normal, b_het_xtreme, b_fm100_normal, b_fm100_xtreme, b_preFire_ndvi, b_topo_roughness, b_pahl, b_intercept_prior, b_het_normal_prior, b_het_xtreme_prior, b_fm100_normal_prior, b_fm100_xtreme_prior, b_preFire_ndvi_prior, b_topo_roughness_prior, b_pahl_prior)
+}
 
+betas_1 <- get_beta_coef(m = m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm, fm100_s_means)
+betas_2 <- get_beta_coef(m = m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm, fm100_s_means)
+betas_3 <- get_beta_coef(m = m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm, fm100_s_means)
+betas_4 <- get_beta_coef(m = m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm, fm100_s_means)
 
-# one_pixel_neighbornood
-### Posterior distributions
-# Extract all samples and then just select the ones for the beta coefficients
-m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm_samps <- posterior_samples(m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm)
-m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm_betas <- m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm_samps[, 1:11]
-
-# Matrix multiply the samples by the desired combination of Betas to assess effect of heterogeneity in each fuel moisture condition
-het_normal_sevOrNot_extremeFm100AndRawFm100_1_ssBurned <- apply(m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm_betas, 1, function(x) x %*% het_normal)
-het_xtreme_sevOrNot_extremeFm100AndRawFm100_1_ssBurned <- apply(m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm_betas, 1, function(x) x %*% het_xtreme)
-
-### Prior distributions
-# Get samples from the priors for plotting purposes
-m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm_prior <- prior_samples(m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm)
-m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm_prior_betas <- m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm_prior[, -c(1, 3)]
-
-het_normal_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_prior <- apply(m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm_prior_betas, 1, function(x) x %*% het_normal)
-het_xtreme_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_prior <- apply(m_sevOrNot_extremeFm100AndRawFm100_1_ssBurned_brm_prior_betas, 1, function(x) x %*% het_xtreme)
-
-hist(het_normal_sevOrNot_extremeFm100AndRawFm100_1_ssBurned, breaks = 100)
-ecdf(het_normal_sevOrNot_extremeFm100AndRawFm100_1_ssBurned)(0)
-
-hist(het_xtreme_sevOrNot_extremeFm100AndRawFm100_1_ssBurned, breaks = 100)
-ecdf(het_xtreme_sevOrNot_extremeFm100AndRawFm100_1_ssBurned)(0)
-
-hist(het_normal_sevOrNot_extremeFm100AndRawFm100_1_ssBurned - het_xtreme_sevOrNot_extremeFm100AndRawFm100_1_ssBurned, breaks = 100)
-ecdf(het_normal_sevOrNot_extremeFm100AndRawFm100_1_ssBurned - het_xtreme_sevOrNot_extremeFm100AndRawFm100_1_ssBurned)(0)
-
-# two_pixel_neighbornood
-m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm_samps <- posterior_samples(m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm)
-m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm_betas <- m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm_samps[, 1:11]
-
-het_normal_sevOrNot_extremeFm100AndRawFm100_2_ssBurned <- apply(m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm_betas, 1, function(x) x %*% het_normal)
-het_xtreme_sevOrNot_extremeFm100AndRawFm100_2_ssBurned <- apply(m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm_betas, 1, function(x) x %*% het_xtreme)
-
-### Prior distributions
-m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm_prior <- prior_samples(m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm)
-m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm_prior_betas <- m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm_prior[, 1:11]
-
-het_normal_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_prior <- apply(m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm_prior_betas, 1, function(x) x %*% het_normal)
-het_xtreme_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_prior <- apply(m_sevOrNot_extremeFm100AndRawFm100_2_ssBurned_brm_prior_betas, 1, function(x) x %*% het_xtreme)
-
-hist(het_normal_sevOrNot_extremeFm100AndRawFm100_2_ssBurned, breaks = 100)
-ecdf(het_normal_sevOrNot_extremeFm100AndRawFm100_2_ssBurned)(0)
-
-hist(het_xtreme_sevOrNot_extremeFm100AndRawFm100_2_ssBurned, breaks = 100)
-ecdf(het_xtreme_sevOrNot_extremeFm100AndRawFm100_2_ssBurned)(0)
-
-hist(het_normal_sevOrNot_extremeFm100AndRawFm100_2_ssBurned - het_xtreme_sevOrNot_extremeFm100AndRawFm100_2_ssBurned, breaks = 100)
-ecdf(het_normal_sevOrNot_extremeFm100AndRawFm100_2_ssBurned - het_xtreme_sevOrNot_extremeFm100AndRawFm100_2_ssBurned)(0)
-
-
-# three_pixel_neighbornood
-m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm_samps <- posterior_samples(m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm)
-m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm_betas <- m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm_samps[, 1:11]
-
-het_normal_sevOrNot_extremeFm100AndRawFm100_3_ssBurned <- apply(m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm_betas, 1, function(x) x %*% het_normal)
-het_xtreme_sevOrNot_extremeFm100AndRawFm100_3_ssBurned <- apply(m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm_betas, 1, function(x) x %*% het_xtreme)
-
-m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm_prior <- prior_samples(m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm)
-m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm_prior_betas <- m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm_prior[, 1:11]
-
-het_normal_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_prior <- apply(m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm_prior_betas, 1, function(x) x %*% het_normal)
-het_xtreme_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_prior <- apply(m_sevOrNot_extremeFm100AndRawFm100_3_ssBurned_brm_prior_betas, 1, function(x) x %*% het_xtreme)
-
-hist(het_normal_sevOrNot_extremeFm100AndRawFm100_3_ssBurned, breaks = 100)
-ecdf(het_normal_sevOrNot_extremeFm100AndRawFm100_3_ssBurned)(0)
-
-hist(het_xtreme_sevOrNot_extremeFm100AndRawFm100_3_ssBurned, breaks = 100)
-ecdf(het_xtreme_sevOrNot_extremeFm100AndRawFm100_3_ssBurned)(0)
-
-hist(het_normal_sevOrNot_extremeFm100AndRawFm100_3_ssBurned - het_xtreme_sevOrNot_extremeFm100AndRawFm100_3_ssBurned, breaks = 100)
-ecdf(het_normal_sevOrNot_extremeFm100AndRawFm100_3_ssBurned - het_xtreme_sevOrNot_extremeFm100AndRawFm100_3_ssBurned)(0)
-
-
-# four_pixel_neighborhood
-m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_samps <- posterior_samples(m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm)
-m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_betas <- m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_samps[, 1:11]
-
-het_normal_sevOrNot_extremeFm100AndRawFm100_4_ssBurned <- apply(m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_betas, 1, function(x) x %*% het_normal)
-het_xtreme_sevOrNot_extremeFm100AndRawFm100_4_ssBurned <- apply(m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_betas, 1, function(x) x %*% het_xtreme)
-
-m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_prior <- prior_samples(m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm)
-m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_prior_betas <- m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_prior[, 1:11]
-
-het_normal_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_prior <- apply(m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_prior_betas, 1, function(x) x %*% het_normal)
-het_xtreme_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_prior <- apply(m_sevOrNot_extremeFm100AndRawFm100_4_ssBurned_brm_prior_betas, 1, function(x) x %*% het_xtreme)
-
-hist(het_normal_sevOrNot_extremeFm100AndRawFm100_4_ssBurned, breaks = 100)
-ecdf(het_normal_sevOrNot_extremeFm100AndRawFm100_4_ssBurned)(0)
-
-hist(het_xtreme_sevOrNot_extremeFm100AndRawFm100_4_ssBurned, breaks = 100)
-ecdf(het_xtreme_sevOrNot_extremeFm100AndRawFm100_4_ssBurned)(0)
-
-hist(het_normal_sevOrNot_extremeFm100AndRawFm100_4_ssBurned - het_xtreme_sevOrNot_extremeFm100AndRawFm100_4_ssBurned, breaks = 100)
-ecdf(het_normal_sevOrNot_extremeFm100AndRawFm100_4_ssBurned - het_xtreme_sevOrNot_extremeFm100AndRawFm100_4_ssBurned)(0)
