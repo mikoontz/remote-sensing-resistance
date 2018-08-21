5550a5cf40643cd5feaa3f7ccb6a2ead3644eae5
Michael Koontz <mikoontz@gmail.com>
2018-08-21 17:37:01 -0600

    initial commit for reproducible workflow

---
+++ data/data_carpentry/convert_jepson-ecoregions.R
@@ -0,0 +1,27 @@
+# Script to convert .kml version of Jepson ecoregion designations into a
+# usable outline of the Sierra Nevada
+
+library(sf)
+library(dplyr)
+library(ggplot2)
+library(raster)
+library(lwgeom)
+
+jep <- st_read("data/features/jepcodes-v7.kml")
+glimpse(jep)
+
+sn_names <- c("nSNF", "cSNF", "sSNF", "nSNH", "cSNH", "sSNH", "Teh")
+
+# Using the "squeeze" approach to get rid of slivers between neighboring polygons
+# https://github.com/r-spatial/sf/issues/547
+
+sn <- 
+  jep %>% 
+  dplyr::filter(Name %in% sn_names) %>% 
+  st_transform(3310) %>% 
+  st_buffer(0.0001) %>%
+  st_difference(.) %>%
+  st_union() %>% 
+  st_transform(4326)
+
+st_write(sn, dsn = "data/features/SierraEcoregion_Jepson/SierraEcoregion_Jepson.shp")
