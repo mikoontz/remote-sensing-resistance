98cc35ab5b3666d7fa6b52652f34e151a12f2b5b
Michael Koontz <mikoontz@gmail.com>
2018-08-21 17:42:00 -0600

    simplify the mixed conifer polygons. might be useful in the future?

--- data/data_carpentry/create_conifer-mask.R
+++ data/data_carpentry/create_conifer-mask.R
@@ -2,6 +2,7 @@ library(raster)
 library(dplyr)
 library(sf)
 library(fasterize)
+library(rgdal)
 
 r <- raster("data/features/sierra_nevada_30m_landsat_template.tif")
 sn <- st_read("data/features/SierraEcoregion_Jepson/") %>%
@@ -46,3 +47,51 @@ sn_r_masked <- merge(nsn_r_masked, ssn_r_masked)
 plot(sn_r_masked)
 
 writeRaster(x = sn_r_masked, filename = "data/data_output/landcover_PFR/mixed_conifer_sn-mask_10-fold-res.tif")
+
+# Turn raster image of mixed conifer into a more simple multipolygon
+# Code from https://johnbaumgartner.wordpress.com/2012/07/26/getting-rasters-into-shape-from-r/
+
+gdal_polygonizeR <- function(x, outshape=NULL, gdalformat = 'ESRI Shapefile',
+                             pypath=NULL, readpoly=TRUE, quiet=TRUE) {
+  if (isTRUE(readpoly)) require(rgdal)
+  if (is.null(pypath)) {
+    pypath <- Sys.which('gdal_polygonize.py')
+  }
+  if (!file.exists(pypath)) stop("Can't find gdal_polygonize.py on your system.")
+  owd <- getwd()
+  on.exit(setwd(owd))
+  setwd(dirname(pypath))
+  if (!is.null(outshape)) {
+    outshape <- sub('\\.shp$', '', outshape)
+    f.exists <- file.exists(paste(outshape, c('shp', 'shx', 'dbf'), sep='.'))
+    if (any(f.exists))
+      stop(sprintf('File already exists: %s',
+                   toString(paste(outshape, c('shp', 'shx', 'dbf'),
+                                  sep='.')[f.exists])), call.=FALSE)
+  } else outshape <- tempfile()
+  if (is(x, 'Raster')) {
+    require(raster)
+    writeRaster(x, {f <- tempfile(fileext='.tif')})
+    rastpath <- normalizePath(f)
+  } else if (is.character(x)) {
+    rastpath <- normalizePath(x)
+  } else stop('x must be a file path (character string), or a Raster object.')
+  system2('python', args=(sprintf('"%1$s" "%2$s" -f "%3$s" "%4$s.shp"',
+                                  pypath, rastpath, gdalformat, outshape)))
+  if (isTRUE(readpoly)) {
+    shp <- readOGR(dirname(outshape), layer = basename(outshape), verbose=!quiet)
+    return(shp)
+  }
+  return(NULL)
+}
+
+ypmc <- raster("data/data_output/landcover_PFR/mixed_conifer.tif")
+# This process takes ages, so comment out the polygonizing code!
+# ypmc_vector <- gdal_polygonizeR(ypmc, readpoly = FALSE)
+
+###
+
+# Find temporary file written to disk and rename it, essentially. These lines won't work in the future.
+# ypmc_vector <- st_read("data/data_output/landcover_PFR/mixed_conifer_simple/file571241732d7a.shp")
+# plot(ypmc_vector$geometry)
+# st_write(obj = ypmc_vector, dsn = "data/data_output/landcover_PFR/mixed_conifer_simple_vector", driver = "ESRI Shapefile")
