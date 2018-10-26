834acb2af9be514cac3d6d3e2f86e3203358e293
Michael Koontz <mikoontz@gmail.com>
2018-10-26 16:17:31 -0600

    clean up the script to just have code for the analysis

--- analyses/mixed-conifer-system-area.R
+++ analyses/mixed-conifer-system-area.R
@@ -7,13 +7,6 @@ mixed_con <- st_read("data/data_output/landcover_PFR/mixed_conifer/mixed_conifer
 area <- st_area(mixed_con)
 
 total_area <- sum(area)
-?units
-class(area)
-
-sum(area)
-?ud_units
-with(ud_units, ha)
-help(package = "units")
 
 units(total_area) <- ud_units$ha
 total_area
