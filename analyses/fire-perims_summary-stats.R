db8d8b2b73bd1041df029708a8f7ceef9a1406de
Michael Koontz <mikoontz@gmail.com>
2018-12-10 10:02:41 -0700

    clean up script and add all the R5 versus FRAP comparisons here (for use in the paper)

--- analyses/fire-perims_summary-stats.R
+++ analyses/fire-perims_summary-stats.R
@@ -29,19 +29,37 @@ total_conifer_samps <-
 s <-
   ss_burned %>%
   filter(conifer_forest == 1) %>% 
-  group_by(fire_id, year, gis_acres, agency, alarm_date, cont_date, cause, comments, date, fire_name, fire_num, inc_num, objective, ordinal_day, state, unit_id, c_method, report_ac) %>% 
+  group_by(fire_id, year, gis_acres, agency, alarm_date, cont_date, cause, comments, date, fire_name, fire_num, inc_num, objective, ordinal_day, state, unit_id, c_method, report_ac, mxd_cn_) %>% 
   nest()
-
-total_conifer_fires <- nrow(s)
-conifer_year_range <- range(s$year)
-
-total_fires_usfs <- 
-  r5_sn_mc %>% 
-  filter(mixed_con_pixels != 0) %>% 
-  nrow()
-unique(r5_sn_mc$FIRE_YEAR)
 
 # Approxmiately many fires would be included under the Steel et al. 2018
 # criteria (> 50% burning in yellow pine mixed conifer)
 r5_sn_mc %>% 
   filter((mixed_con_pixels * 30 * 30) > (0.5 * as.numeric(st_area(.))))
+
+# Area and number of fires comparison between USFS Region 5 geospatial dataset and FRAP-derived dataset
+
+# This is the number of Region 5 fires that burned at least partially in YPMC
+r5_fire_count_ypmc <- 
+  r5_sn_mc %>% 
+  filter(mixed_con_pixels != 0) %>% 
+  nrow()
+
+# This is the area of yellow pine/mixed-conifer covered by the Region 5 perimeters
+# in hectares
+r5_area_ypmc_burned <-
+  r5_sn_mc %>% 
+  summarize(total_area_ypmc = sum(mixed_con_pixels) * 30 * 30 / 10000) %>% 
+  pull(total_area_ypmc)
+
+# This is the number of FRAP-derived fires (our algorithm) that burned at least
+# partially in 
+frap_fire_count_ypmc <- nrow(s)
+
+frap_area_ypmc_burned <-
+  s %>% 
+  summarize(total_area_ypmc = sum(mxd_cn_) * 30 * 30 / 10000) %>% 
+  pull(total_area_ypmc)
+
+# How many more hectares are covered by our new dataset?
+(frap_area_ypmc_burned - r5_area_ypmc_burned)
