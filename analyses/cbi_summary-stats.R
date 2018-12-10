ffd43c1289f74e362c42ee3b6727bea8b5534ed4
Michael Koontz <mikoontz@gmail.com>
2018-12-10 08:36:49 -0700

    be more specific that these variables describe the CBI plot data-- helps when writing the manuscript and including these variable names in the text

--- analyses/cbi_summary-stats.R
+++ analyses/cbi_summary-stats.R
@@ -9,12 +9,12 @@ library(here)
 cbi_48_bicubic <- st_read(here::here("data/ee_cbi-calibration/cbi-calibration_48-day-window_L57_bicubic-interp.geojson"), stringsAsFactors = FALSE)
 cbi_48_bicubic$date <- as.POSIXct(cbi_48_bicubic$date / 1000, origin="1970-01-01")
 
-conifer_only <- subset(cbi_48_bicubic, subset = conifer_forest == 1)
+conifer_only_cbi <- subset(cbi_48_bicubic, subset = conifer_forest == 1)
 
 total_plots <- nrow(cbi_48_bicubic)
-total_conifer_plots <- nrow(conifer_only)
+total_conifer_cbi_plots <- nrow(conifer_only_cbi)
 
-total_conifer_fires <- n_distinct(conifer_only$fire_name)
-total_conifer_years <- n_distinct(year(conifer_only$date))
+total_conifer_cbi_fires <- n_distinct(conifer_only_cbi$fire_name)
+total_conifer_cbi_years <- sort(unique(year(conifer_only_cbi$date)))
 
-sort(unique(year(conifer_only$date)))
+sort(unique(year(conifer_only_cbi$date)))
