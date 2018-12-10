f11ff71c3962d115f6736299afcbf89e507fe98b
Michael Koontz <mikoontz@gmail.com>
2018-12-10 10:10:08 -0700

    streamline the merge to get rid of lines that should really be part of "configure". Also, commenting out the primary part of this code because I was able to export all samples in a single file. May need to re-implement this code if samples come as separate files (for instance, when trying to include the texture measurements too).

--- data/data_carpentry/merge_fire-samples.R
+++ data/data_carpentry/merge_fire-samples.R
@@ -4,19 +4,13 @@ library(sf)
 library(dplyr)
 library(purrr)
 
-samps_files <- list.files(path = here::here("data/ee_fire-samples"), full.names = TRUE)
-samps_list <- 
-  samps_files %>% 
-  map(.f = function(file_name) {
-    st_read(file_name)
-  })
-
-samps <- do.call(rbind, samps_list)
-
-# Extract unique Fire ID from the sample point IDs
-samps$fire_id <- substr(as.character(samps$id), start = 1, stop = 20)
-samps$samp_id <- as.numeric(substr(as.character(samps$id), start = 22, stop = nchar(as.character(samps$id))))
-samps$year <- as.numeric(as.character(samps$year_))
-samps <- dplyr::select(samps, -year_)
+# samps_files <- list.files(path = here::here("data/ee_fire-samples"), full.names = TRUE)
+# samps_list <- 
+#   samps_files %>% 
+#   map(.f = function(file_name) {
+#     st_read(file_name)
+#   })
+# 
+# samps <- do.call(rbind, samps_list)
 
 saveRDS(samps, file = here::here("data/data_output/all-fire-samples.rds"))
