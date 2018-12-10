d6d9279cd2a88226e8eaafc0b3cde024517ccdd2
Michael Koontz <mikoontz@gmail.com>
2018-12-10 10:07:03 -0700

    initial code to rename bands in the exported database of implementation of our algorithm on the FRAP database

--- data/data_carpentry/process-frap-derived-imagery.R
+++ data/data_carpentry/process-frap-derived-imagery.R
@@ -4,17 +4,26 @@ library(rasterVis)
 
 band_names <- c('rdnbr', 'nbr', 'nbr_1', 'rdndvi', 'rbr', 'ndvi', 'ndvi_1', 'het_ndvi_1', 'nbhd_ndvi_1', 'het_ndvi_2', 'nbhd_ndvi_2', 'het_ndvi_3', 'nbhd_ndvi_3', 'het_ndvi_4', 'nbhd_ndvi_4', 'constant', 'constant_1', 'constant_2', 'constant_3', 'constant_4', 'longitude', 'latitude', 'ypmc', 'slope', 'aspect', 'topo_roughness_1', 'topo_roughness_2', 'topo_roughness_3', 'topo_roughness_4', 'elevation', 'B1_pre', 'B2_pre', 'B3_pre', 'B4_pre', 'B5_pre', 'B6_pre', 'B7_pre', 'B1_post', 'B2_post', 'B3_post', 'B4_post', 'B5_post', 'B6_post', 'B7_post', 'preFerc', 'preFfm100', 'preFvpd', 'earlyFvs', 'earlyFhdw')
 
-fire1 <- raster::stack("data/ee_fire-images/19880704_00017_0000395195b1272a72a0_epsg3310.tif")
+fire <- raster::stack("data/ee_fire-images/19880704_00017_0000395195b1272a72a0_epsg3310.tif")
+fire <- raster::stack("data/ee_fire-images/19870829_00059_0000de101e16afb630c0_epsg3310.tif")
+fire <- raster::stack("data/ee_fire-images/19870830_00045_0000bafc4812e81fdc1f_epsg3310.tif") # Hamm Fire
+fire <- raster::stack("data/ee_fire-images/19870830_00177_0000f8d62e28cf5fa43c_epsg3310.tif")
+fire <- raster::stack("data/ee_fire-images/19870830_00421_00003a70ebf6fedca59e_epsg3310.tif")
 
-names(fire1) <- band_names
+names(fire) <- band_names
 # Overall RBR values from the model
-min_rbr <- 0.041191844
-max_rbr <- 0.2836425
+burned_unburned_rbr_threshold <- 0.041191844
+high_sev_low_sev_rbr_threshold <- 0.2836425
+
 # A severity palette
 RBR_viz <- c('#008000', '#ffff00', '#ffA500', '#ff0000')
 
-# rbr_stats <- cellStats(fire1[["rbr"]], range)
-breaks <- seq(min_rbr, max_rbr, length.out = 100)
-cols <- colorRampPalette(RBR_viz)(length(breaks)-1)
+breaks <- c(cellStats(fire[["rbr"]], min), seq(burned_unburned_rbr_threshold, high_sev_low_sev_rbr_threshold, length.out = 100), cellStats(fire[["rbr"]], max))
+# cols <- colorRampPalette(c('#008000', RBR_viz, '#ff0000'))
+cols <- colorRampPalette(RBR_viz)
+levelplot(fire[["rbr"]], at = breaks, col.regions = cols)
 
-levelplot(fire1[["rbr"]], col.regions = cols)
+breaks <- c(cellStats(fire[["rbr"]], min), seq(burned_unburned_rbr_threshold, cellStats(fire[["rbr"]], max), length.out = 100))
+cols <- colorRampPalette(RBR_viz)
+
+levelplot(fire[["rbr"]], at = breaks, col.regions = cols)
